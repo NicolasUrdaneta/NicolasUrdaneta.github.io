bfbe9c9a61c01397e66e83332416b0279a743ed7
Nicolás Urdaneta <53277828+Nurdaneta@users.noreply.github.com>
2022-01-17 20:08:09 -0500

    Fixes in read more option

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
+/* READ MORE */
+
+let noOfCharac = 300;
+let contents = document.querySelectorAll(".content");
+contents.forEach(content => {
+    //If text length is less that noOfCharac... then hide the read more button
+    if(content.textContent.length < noOfCharac){
+        content.nextElementSibling.style.display = "none";
+    }
+    else{
+        let displayText = content.textContent.slice(0,noOfCharac);
+        let moreText = content.textContent.slice(noOfCharac);
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
+});
+
+function readMore(btn){
+    let post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Show full abstract" ? btn.textContent = "Hide abstract" : btn.textContent = "Show full abstract";
+}
