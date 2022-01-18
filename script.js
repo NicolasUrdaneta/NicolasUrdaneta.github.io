1ba893a4bb55ef4c9d35a6105f9e1723e6ea4af2
Nicolás Urdaneta <53277828+Nurdaneta@users.noreply.github.com>
2022-01-18 11:47:02 -0500

    Fixes in read more option

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ contents.forEach(content => {
     }
 });
 
-$( document ).on( "pagecreate", "#aboutPage", function readMore(btn){
+function readMore(btn){
     let post = btn.parentElement;
     post.querySelector(".dots").classList.toggle("hide");
     post.querySelector(".more").classList.toggle("hide");
     btn.textContent == "Show full abstract" ? btn.textContent = "Hide abstract" : btn.textContent = "Show full abstract";
-});
+}
 
 
