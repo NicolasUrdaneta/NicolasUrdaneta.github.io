1815d4aab9205ba4cfdbf1267d595b738c3d57c6
Nicolás Urdaneta <urdaneta.nicolas@yahoo.com>
2024-04-20 13:43:08 -0400

    Update in data page

--- script.js
+++ script.js
@@ -54,4 +54,26 @@ function readMore(btn){
 };
 
 
-        
+/* TRIANGLE TO DISPLAY MORE CONTENT */ 
+function readMoreTriangle(container) {
+    var text = container.querySelector('.hidden-text');
+    var triangle = container.querySelector('.triangle-button');
+
+    // Check if the click target is inside the hidden text
+    if (event.target.closest('.hidden-text')) {
+        event.stopPropagation();  // Prevent the click event propagation
+        return;  // Exit the function without toggling the visibility
+    }
+
+
+
+    if (text.style.display === "none" || text.style.display === "") {
+        text.style.display = "block";
+        triangle.classList.add('down');
+        triangle.classList.add('gray');
+    } else {
+        text.style.display = "none";
+        triangle.classList.remove('down');
+        triangle.classList.remove('gray');
+    }
+}
