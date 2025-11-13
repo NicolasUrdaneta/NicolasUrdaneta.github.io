dad4b59c7850136d7d90d147285e0b5f7135dc22
Nicolás Urdaneta <urdaneta.nicolas@yahoo.com>
2025-11-13 12:50:58 -0500

    Research updates, design, typos

--- script.js
+++ script.js
@@ -24,34 +24,17 @@ function closeMenu() {
 
 /* READ MORE */
 
-function init () {
 
-        let noOfCharac = 0;
-        let contents = document.querySelectorAll(".content");
-        
-    contents.forEach(content => {
-            //If text length is less that noOfCharac... then hide the read more button
-            if(content.textContent.length < noOfCharac){
-                content.nextElementSibling.style.display = "none";
-            }
-            else{
-                let displayText = content.textContent.slice(0,noOfCharac);
-                let moreText = content.textContent.slice(noOfCharac);
-                content.innerHTML = `${displayText}<span class="dots"></span><span class="hide more">${moreText}</span>`;
-            }
-        });
+function readMore(btn) {
+  const abstract = btn.previousElementSibling; // assumes <button> follows <p class="content">
+  abstract.classList.toggle("show");
 
-    }
-
-init();
-document.addEventListener('swup:contentReplaced', init);   
-
-function readMore(btn){
-    let post = btn.parentElement;
-    post.querySelector(".dots").classList.toggle("hide");
-    post.querySelector(".more").classList.toggle("hide");
-    btn.textContent == "Show abstract" ? btn.textContent = "Hide abstract" : btn.textContent = "Show abstract";
-};
+  if (abstract.classList.contains("show")) {
+    btn.textContent = "Hide abstract";
+  } else {
+    btn.textContent = "Show abstract";
+  }
+}
 
 
 /* TRIANGLE TO DISPLAY MORE CONTENT */ 
